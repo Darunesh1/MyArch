aded9aa8839551f523266ab0351c0f3b71d8569d
Darunesh R <sathyarajasekar5873@gmail.com>
2026-03-23 00:37:02 +0530

    script to rotate wallpaper

---
+++ scripts/swww-cycle.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define the directory and the interval (in seconds)
+WALLPAPER_DIR="/home/darriour/Pictures/Wallpaper"
+INTERVAL=200 
+
+while true; do
+    # Find all images in the folder and pick a random one
+    IMG=$(find "$WALLPAPER_DIR" -type f \( -iname \*.jpg -o -iname \*.png -o -iname \*.jpeg \) | shuf -n 1)
+    
+    # Apply the wallpaper with a smooth transition
+    # You can change the transition-type to: simple, fade, left, right, top, bottom, wipe, wave, grow, center
+    swww img "$IMG" --transition-fps 60 --transition-type random --transition-duration 2
+    
+    # Wait for the specified interval before changing again
+    sleep $INTERVAL
+done
